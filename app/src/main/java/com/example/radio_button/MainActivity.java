7c27ea1d33c96e327292802330550e8f36ba22eb
manahil <bitf18a507@pucit.edu.pk>
2021-12-12 18:46:17 +0500

    fifth

--- app/src/main/java/com/example/radio_button/MainActivity.java
+++ app/src/main/java/com/example/radio_button/MainActivity.java
@@ -3,18 +3,41 @@ package com.example.radio_button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup radiogroup;
-    RadioButton radiobtton;
+   Button btton;
     TextView textview;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        radiogroup=findViewById(R.id.radi);
+       btton=findViewById(R.id.button);
+       textview=findViewById(R.id.txt);
+       btton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+
+
+
+    }
+    public void CheckButton(View v)
+    {
+        int radioseselected=radiogroup.getCheckedRadioButtonId();
+        RadioButton radiobttn=findViewById(radioseselected);
+
+        Toast.makeText(this,"you selected:"+radiobttn.getText(),Toast.LENGTH_SHORT);
+
     }
 }
