c9a932e53c09aa4c6887c563292e803575a9414a
manahil <bitf18a507@pucit.edu.pk>
2021-12-12 18:36:40 +0500

    fourth

--- app/src/main/java/com/example/radio_button/MainActivity.java
+++ app/src/main/java/com/example/radio_button/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.radio_button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioGroup radiogroup;
+    RadioButton radiobtton;
+    TextView textview;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
